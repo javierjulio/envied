e53c7c39ec7a7998ed1489614546ab125e6b73fb
Javier Julio <jjfutbol@gmail.com>
2026-09-05 17:03:35 -0400

    Update simplecov dependencies
    Remove the deprecated add_filter, no need to replace since doesn't impact anything in the final coverage report.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ if ENV.fetch("COVERAGE", false)
   require "simplecov"
   require "simplecov-cobertura"
   SimpleCov.start do
-    add_filter %r{^/spec/}
     formatter SimpleCov::Formatter::CoberturaFormatter
   end
 end
